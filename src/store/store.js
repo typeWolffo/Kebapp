e18854795fd5dbab260e86828058389d8024c55c
Jakub Wilk <jwilk436@gmail.com>
2022-05-24 11:24:23 +0200

    Update dependencies due to CVE-2021-3803

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../slices/auth";
 import messageReducer from "../slices/message";
+import eventReducer from "../slices/event";
 
 const reducer = {
   auth: authReducer,
+  event: eventReducer,
   message: messageReducer,
 };
 
